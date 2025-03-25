01d8969817be6f950201de46810d576dc1478805
Moon <5924350+MatrikMoon@users.noreply.github.com>
2025-03-25 04:42:22 -0500

    Bump version numbers

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 export const masterAddress = "server.tournamentassistant.net";
 export const masterPort = "8676";
-export const version = "1.1.10";
-export const versionCode = 1110;
+export const version = "1.1.11";
+export const versionCode = 1111;
